33ff50ad53bb3642e777e01316f72e35de1cdb8f
Penguino <heriberto@melo.nyc>
2018-08-26 22:19:14 -0400

    Testing commits from Surface

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var port = 3000;
 var app = express();
 
 
-//testing commits
+//testing commits to see if surface works
 //View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
